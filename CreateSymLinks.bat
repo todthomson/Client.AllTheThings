55c176a48d9e191ea35ba176c26a426bd8981544
Tod Thomson <todthomson@me.com>
2015-10-06 14:04:24 +1000

    what the symlinks batch file should look like

---
+++ CreateSymLinks.bat
@@ -0,0 +1,3 @@
+mklink /D Client.Microservice1.Service ..\Client.Microservice1\Client.Microservice1.Service
+mklink /D Client.Microservice2.Service ..\Client.Microservice2\Client.Microservice2.Service
+mklink /D Client.Microservice3.Service ..\Client.Microservice3\Client.Microservice3.Service
